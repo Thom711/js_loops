a5cb1af6b644be2a501b6f67b53cf261b1386344
Thom711 <59656747+Thom711@users.noreply.github.com>
2020-12-18 12:04:09 +0100

    -final

---
+++ even-odd-reporter.js
@@ -0,0 +1,9 @@
+for(i = 1; i <= 20; i++) {
+    let check = i % 2;
+
+    if(!check) {
+        console.log(i + ' is even.');
+    } else {
+        console.log(i + ' is oneven.');
+    }
+}
---
+++ multiplication_tables.js
@@ -0,0 +1,6 @@
+for(table = 1; table <= 10; table++) {
+    for(i = 1; i <= 10; i++) {
+        let result = i * table;
+        console.log(i + ' x ' + table + ' is ' + result + '.');
+    }
+}
